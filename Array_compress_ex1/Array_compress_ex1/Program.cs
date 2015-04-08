66083c9c8d77795b41009ec6f812d734913e74b9
art-larry <art.mystep@gmail.com>
2015-04-08 12:22:06 +0300

    1st exercise(2nd attempt)

---
+++ Array_compress_ex1/Array_compress_ex1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Array_compress_ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rnd = new Random();
+            int[] arr = new int[20];
+
+            for (int i = 0; i < arr.Length; ++i)
+            {
+                arr[i] = rnd.Next(-10, 10);
+            }
+            arr[arr.Length - 1] = 0;
+            foreach (var el in arr)  // контрольный вывод
+            {
+                Console.Write(el + " ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < arr.Length; ++i)
+            {
+                if (arr[i] == 0)
+                {
+                    for (int j = i + 1; j < arr.Length; ++j)
+                    {
+                        arr[j - 1] = arr[j];
+                    }
+                    arr[arr.Length - 1] = -1;
+                }
+            }
+
+            foreach (var el in arr)  // результирующий вывод
+            {
+                Console.Write(el + " ");
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
