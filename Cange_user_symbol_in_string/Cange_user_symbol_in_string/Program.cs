f32e4b226ecd8457787ea592912fdd74d0df75ce
art-larry <art.mystep@gmail.com>
2015-04-08 17:20:03 +0300

    Change user symbol in string ex6

---
+++ Cange_user_symbol_in_string/Cange_user_symbol_in_string/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Cange_user_symbol_in_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string workString = null;
+            char userSymbol = char.MinValue;
+            workString = String.Join(" ", args);
+            Console.WriteLine(workString);
+            
+            do
+            {
+                Console.Write("Enter one symbol:");
+                string tmp = Console.ReadLine();
+                if(Char.TryParse(tmp,out userSymbol))
+                {
+                    break;
+                }
+            }
+            while (true);
+
+            char[] chArray = workString.ToCharArray();
+            for(int i = 0; i < chArray.Length; ++i)
+            {
+                if(chArray[i] == userSymbol)
+                {
+                    chArray[i] = Char.ToUpper(userSymbol);
+                }
+            }
+            workString = new String(chArray);
+            int c = workString.LastIndexOf(Char.ToUpper(userSymbol));
+            workString = workString.Remove(c);
+
+            Console.WriteLine(workString);
+        }
+    }
+}
