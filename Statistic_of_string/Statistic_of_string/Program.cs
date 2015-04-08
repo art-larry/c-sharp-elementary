b37d565b26c0329677f67f703bf2c718da301169
art-larry <art.mystep@gmail.com>
2015-04-08 16:40:40 +0300

    Statistic of string ex5

---
+++ Statistic_of_string/Statistic_of_string/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Statistic_of_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string workString = null;
+            if(args.Length > 0)
+            {
+                foreach(var el in args)
+                {
+                    Console.Write(el + " ");
+                }
+                Console.WriteLine();
+                workString = String.Join(" ", args);
+            }
+
+            Console.WriteLine("Quantity of symbol:" + workString.Length);
+            Console.WriteLine("Quantity space symbol:" + (args.Length - 1));
+            char[] tmp = workString.ToCharArray();
+            int upperCase = 0, lowerCase = 0, digit = 0;
+            foreach(var el in tmp)
+            {
+                if(el >= 'A' && el <= 'Z' || el >= 'А' && el <= 'Я')
+                {
+                    ++upperCase;
+                }
+                if(el >= 'a' && el <= 'z' || el >= 'а' && el <= 'я')
+                {
+                    ++lowerCase;
+                }
+                if(el >= '0' && el <= '9')
+                {
+                    ++digit;
+                }
+            }
+            Console.WriteLine("Quantity upper case:" + upperCase);
+            Console.WriteLine("Quantity lower case:" + lowerCase);
+            Console.WriteLine("Total alphabet symbol:" + (upperCase + lowerCase));
+            Console.WriteLine("Digits quantity:" + digit);
+        }
+    }
+}
