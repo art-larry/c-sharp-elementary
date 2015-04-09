4082575e3f6fa04257f7511e9be2a4cca167741c
art-larry <art.mystep@gmail.com>
2015-04-09 20:41:15 +0300

    struct for coeff ex2.1

---
+++ Struct_for_save_koef_linear_equaision_ex2_1/Struct_for_save_koef_linear_equaision_ex2_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Struct_for_save_koef_linear_equaision_ex2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var inst1 = new Coefficients(2.0, 9.4);
+                var inst2 = new Coefficients();
+                inst2.Parse("2.9, 4.3");
+                Console.WriteLine("coefa: {0}, coefb: {1}", inst1.CoefA, inst1.CoefB);
+                Console.WriteLine("CoefA: {0}, CoefB: {1}", inst2.CoefA, inst2.CoefB);
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Struct_for_save_koef_linear_equaision_ex2_1/Struct_for_save_koef_linear_equaision_ex2_1/coefficients.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace Struct_for_save_koef_linear_equaision_ex2_1
+{
+    struct Coefficients
+    {
+        private double coefA;
+        private double coefB;
+
+        public Coefficients(double a, double b)
+        {
+            coefA = a;
+            coefB = b;
+        }
+
+        public double CoefA
+        {
+            get { return coefA; }
+            private set { coefA = value; }
+        }
+
+        public double CoefB
+        {
+            get { return coefB; }
+            private set { coefB = value; }
+        }
+
+        public void Parse(string param)
+        {
+            
+            string[] inputParam = param.Split(new char[] {',', ' '}, 2, StringSplitOptions.RemoveEmptyEntries);
+            if(!Double.TryParse(inputParam[0], NumberStyles.AllowDecimalPoint, CultureInfo.CreateSpecificCulture("en-en"), out coefA) 
+                & !Double.TryParse(inputParam[1], NumberStyles.AllowDecimalPoint, CultureInfo.CreateSpecificCulture("en-en"), out coefB))
+            {
+                throw new FormatException();
+            }
+        }
+    }
+}
