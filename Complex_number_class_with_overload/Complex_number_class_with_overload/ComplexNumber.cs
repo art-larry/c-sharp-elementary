3e0b14878d4d62e6a2a3252c1ff8cf5bb40f2d56
art-larry <art.mystep@gmail.com>
2015-04-10 10:08:52 +0300

    Complex number class with overload ex2.5

---
+++ Complex_number_class_with_overload/Complex_number_class_with_overload/ComplexNumber.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Complex_number_class_with_overload
+{
+    class ComplexNumber
+    {
+        private double x;
+        private double y;
+
+        public ComplexNumber(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static ComplexNumber operator +(ComplexNumber a, ComplexNumber b)
+        {
+            return new ComplexNumber(a.x + b.x, a.y + b.y);
+        }
+
+        public static ComplexNumber operator -(ComplexNumber a, ComplexNumber b)
+        {
+            return new ComplexNumber(a.x - b.x, a.y - b.y);
+        }
+
+        public static ComplexNumber operator *(ComplexNumber a, ComplexNumber b)
+        {
+            return new ComplexNumber((a.x * b.x) - (a.y * b.y), (a.y * b.x) + (a.x * b.y));
+        }
+
+        public static ComplexNumber operator /(ComplexNumber a, ComplexNumber b)
+        {
+            return new ComplexNumber((a.x * b.x + a.y * b.y) / (b.x * b.x + b.y * b.y), (a.y * b.x - a.x * b.y) / (b.x * b.x + b.y * b.y));
+        }
+
+        public static ComplexNumber operator +(ComplexNumber a, int b)
+        {
+            return new ComplexNumber(a.x + b, a.y);
+        }
+
+        public static ComplexNumber operator +(int a, ComplexNumber b)
+        {
+            return b + a;
+        }
+
+        public static ComplexNumber operator -(ComplexNumber a, int b)
+        {
+            return new ComplexNumber(a.x - b, a.y);
+        }
+
+        public static ComplexNumber operator -(int a, ComplexNumber b)
+        {
+            return new ComplexNumber(a - b.x, b.y);
+        }
+
+        public static ComplexNumber operator *(ComplexNumber a, int b)
+        {
+            return new ComplexNumber(a.x * b, a.y * b);
+        }
+
+        public static ComplexNumber operator *(int a, ComplexNumber b)
+        {
+            return b * a;
+        }
+
+        public static bool operator ==(ComplexNumber a, ComplexNumber b)
+        {
+            return (a.x == b.x) && (a.y == b.y);
+        }
+
+        public static bool operator !=(ComplexNumber a, ComplexNumber b)
+        {
+            return !(a == b);
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} + ({1})i", x, y);
+        }
+    }
+}
---
+++ Complex_number_class_with_overload/Complex_number_class_with_overload/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Complex_number_class_with_overload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ComplexNumber z = new ComplexNumber(1, 1);
+            ComplexNumber z1 = z - (z * z * z - 1) / (3 * z * z);
+            Console.WriteLine("z1 = {0}", z1.ToString());
+        }
+    }
+}
